1b4af91d476e0bc809606a834e9a24a9fa3bab2b
pablonicovillalba2193@gmail.com <pablonicovillalba2193@gmail.com>
2022-10-22 12:39:15 -0300

    Creo formularios Ingresar y Registrarse. Ajusto los link del navbar

---
+++ Angular/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+/*import { AppComponent } from './app.component';*/
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { NosotrosComponent } from './Pages/nosotros/nosotros.component';
+import { ECommerceComponent } from './Pages/eCommerce/eCommerce.component';
+import { IngresarComponent } from './Pages/ingresar/ingresar.component';
+import { RegistrarseComponent } from './Pages/registrarse/registrarse.component';
+import { TurneroComponent } from './Pages/turnero/turnero.component';
+import { PerfilComponent } from './Pages/perfil/perfil.component';
+
+
+const routes: Routes = [
+ /*{path: "", component: AppComponent },*/
+ {path:"inicio", component: InicioComponent},
+ {path:"nosotros", component: NosotrosComponent},
+ {path:"eCommerce", component: ECommerceComponent},
+ {path:"ingresar", component: IngresarComponent},
+ {path:"registrarse", component: RegistrarseComponent},
+ {path:"turnero", component: TurneroComponent},
+ {path:"perfil", component: PerfilComponent}
+];
+@NgModule({
+  
+  imports: [RouterModule.forRoot(routes)],
+  
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
